5e16bad24ad4c6973dc86b90997ed1e666a1ca68
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to save ASPxSplitter's client-side layout after every end-user modification 
+
+
+<p>This example demonstrates how to update client-side layout data of the ASPxSplitter control when a user modifies the control<br /><br /><strong>See Also:<br /><br /><a href="https://www.devexpress.com/Support/Center/p/T152893">T152893: MVC Splitter - How to save the client layout of the control after every modification by an end-user</a><br /></strong></p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,20 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+	End Sub
+	Protected Sub ASPxSplitter1_ClientLayout(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxClasses.ASPxClientLayoutArgs)
+		If e.LayoutMode = DevExpress.Web.ASPxClasses.ClientLayoutMode.Saving Then
+			Session("layoutData") = e.LayoutData
+		ElseIf Session("layoutData") IsNot Nothing Then
+			e.LayoutData = CStr(Session("layoutData"))
+		End If
+	End Sub
+End Class
